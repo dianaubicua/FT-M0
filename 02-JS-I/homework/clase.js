d4e6795221e6ca3a99ffea406450f342f539d7a6
diana <diana.ruiz.bautista@gmail.com>
2021-06-30 19:30:33 -0500

    repaso de tareas

---
+++ 02-JS-I/homework/clase.js
@@ -0,0 +1,14 @@
+var nombre = "Diana";
+let apellido = "Ruiz";
+var edad = "31";
+const humano = true;
+
+nombre = "Francisco";
+apellido = "Junior"
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log(humano);
+
+console.log("Hola Mundo")
